cc10fc10533ef022b2377a3e0a993b2a7ce98e1a
adammichaelwood <adam.michael.wood@gmail.com>
2016-12-07 18:52:35 -0800

    refactor tonus

--- ophis/tonus/pitchclass.py
+++ ophis/tonus/pitchclass.py
@@ -1,84 +1,77 @@
 import sys
 
 
+class PitchClass:
+
+    def __init__(self, essential_set, attrs):
+        for key, value in attrs.items():
+            setattr(self, key, value)
+
+        # The essential set is the musical domain of the pitchclass being created. For example, Conventional Western Music has an essential set of 12 notes.
+        essential_set.add(self)
+        self.essential_set = essential_set
+
+        # When creating a new pitchclass,
+        # you might want to include the following:
+        # name -- the full name, usually ALL CAPS - DSHARP
+        # base -- the base of the name - D
+        # base_num -- the ordinal number of the base, within the set of all bases - 2
+        # base_value -- the absolute value of the base, within the set of all pitchclasses - 3
+        # value -- the ordinal number of the actual pitchclass - 4
+        # mod_value -- the positive or negative value of the modifier - 1
+        # unicode -- for printing
+        # ascii -- for printing
+        # verbose -- for printing
+        # ly -- for outputting to lilypond
+        #
+        # The values attributes above apply for conventional Western music theory and notation, but may or may not apply to other systems.
+        #
+
+
+    def enharmonics(self, include_original=False, **kwargs):
+        enharmonics = self.essential_set.get_enharmonics(self, kwargs)
+        if include_original = False:
+            enharmonics.discard(self)
+        return enharmonics
+
+    def augment(self):
+        return self.essential_set.get_augments(self)
+
+    def diminish(self):
+        return self.essential_set.get_dims(self)
+
+
+class PitchClassSet(set):
+
+    def pitches_by_abs(self, abs_num):
+        pitches = PitchClassSet([])
+        abs_num = abs_num%12
+        for pitch in self:
+            if pitch.abs_num == abs_num:
+                pitches.add(pitch)
+        return pitches
+
+    def sorted_by_mod(self):
+        pitches = list(self)
+        pitches.sort(key=lambda x: abs(x.mod_num))
+        return pitches
+
+class EssentialPitchClassSet(PitchClassSet):
+
+    def get_enharmonics(self, pitchclass):
+        pass
+
+    def get_augments(self, pitchclass):
+        pass
+
+    def get_dims(self, pitchclass):
+        pass
+
+es = PitchClassSet([])
+
 
     #def solfege(self, tonic=C)
     # return string
 
 #old and busted
 pitchclasses = []
-
-
-
-
-
-
-
-
-white_notes = {
- "C" : [0, 0, "do"],
- "D" : [1, 2, "re"],
- "E" : [2, 4, "mi"],
- "F" : [3, 5, "fa"],
- "G" : [4, 7, "sol"],
- "A" : [5, 9, "la"],
- "B" : [6, 11, "ti"]
-}
-
-
-
-module = sys.modules[__name__]
-
-for letter, letter_vals in white_notes.items():
-    mods = {
-        "" : {
-            "value": 0,
-            "ascii": "",
-            "unicode": "",
-            "lilypond": "",
-            "verbose" : "NATURAL"
-
-        },
-        "SHARP" : {
-            "value": 1,
-            "ascii": "#",
-            "unicode": u"\u266F",
-            "lilypond": "is",
-            "verbose" : "SHARP"
-        },
-        "DUBSHARP" : {
-            "value": 2,
-            "ascii": "##",
-            "unicode": u"\U0001D12A",
-            "lilypond": "isis",
-            "verbose" : "DOUBLE SHARP"
-        },
-        "FLAT" : {
-            "value": -1,
-            "ascii": "b",
-            "unicode": u"\u266D",
-            "lilypond": "es",
-            "verbose" : "FLAT"
-        },
-        "DUBFLAT" : {
-            "value": -2,
-            "ascii": "bb",
-            "unicode": u"\U0001D12B",
-            "lilypond": "eses",
-            "verbose" : "DOUBLE FLAT"
-        }
-    }
-    for mod, mod_val in mods.items():
-        pitchclass_name = letter + mod
-        pitchclass_attrs = {
-        "name" : letter + mod,
-        "letter" : letter,
-        "letter_num" : letter_vals[0],
-        "abs_num" : (letter_vals[1] + mod_val["value"])%12,
-        "mod_num" : mod_val["value"],
-        "unicode" : letter + mod_val["unicode"],
-        "ascii" : letter + mod_val["ascii"],
-        "verbose" : letter + " " + mod_val["verbose"],
-        "lilypond" : letter + mod_val["lilypond"]
-        }
-        setattr(module, pitchclass_name, PitchClass(pitchclass_attrs))
